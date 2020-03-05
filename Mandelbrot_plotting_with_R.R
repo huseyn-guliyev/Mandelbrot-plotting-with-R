e776925535a177f0d01fc589ba1fe3c6bbc59ccf
huseyn-guliyev <quliyevhuseyn767@gmail.com>
2020-03-05 23:20:58 +0400

    Mandelbrot plotting

--- Mandelbrot_plotting_with_R.R
+++ Mandelbrot_plotting_with_R.R
@@ -1,5 +1,10 @@
-view <- mandelbrot(xlim = c(-0.8438146, -0.8226294),
-                   ylim = c(0.1963144, 0.2174996), iter = 500)
+install.packages("mandelbrot")
+library(mandelbrot)
+
+
+view <- mandelbrot(xlim = c(-2, 2),
+                   ylim = c(-2, 2), iter = 500) #parameters in ylim and xlim can be any number,
+                                                # recommended range to pick numbers is c(-2, 2)
 
 spectral <- RColorBrewer::brewer.pal(11, "Spectral")
 cols <- mandelbrot_palette(spectral, fold = FALSE)
@@ -10,3 +15,4 @@ blues <- RColorBrewer::brewer.pal(9, "Blues")
 cols <- mandelbrot_palette(blues, in_set = "white",
                            fold = TRUE, reps = 2)
 plot(view, col = cols, transform = "log") 
+
